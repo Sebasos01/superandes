f31e0a9083a523454064359204b95caca7cf9361
Sebastián Ospino <sospino110@gmail.com>
2022-10-16 13:55:28 -0500

    Se agrega interfaz de gerente sucursal

---
+++ src/main/java/uniandes/isis2304/superandes/interfazApp/InterfazGeneral.java
@@ -0,0 +1,448 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Universidad	de	los	Andes	(Bogotá	- Colombia)
+ * Departamento	de	Ingeniería	de	Sistemas	y	Computación
+ * Licenciado	bajo	el	esquema	Academic Free License versión 2.1
+ * 		
+ * Curso: isis2304 - Sistemas Transaccionales
+ * Proyecto: Superandes Uniandes
+ * @version 1.0
+ * @author Sebastián Ospino
+ * @author Ana Sofía Castellanos
+ * Octubre del 2022
+ * 
+ * Revisado por:
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+package uniandes.isis2304.superandes.interfazApp;
+
+import java.awt.Color;
+import java.awt.Desktop;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import javax.jdo.JDODataStoreException;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+import org.apache.log4j.Logger;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+
+import uniandes.isis2304.superandes.negocio.Superandes;
+
+/**
+ * Clase general de la interfaz
+ * @author Sebastián Ospino
+ * @author Ana Sofía Castellanos
+ */
+@SuppressWarnings("serial")
+public abstract class InterfazGeneral extends JFrame implements ActionListener {
+	
+	/* ****************************************************************
+	 * 			Constantes
+	 *****************************************************************/
+	/**
+	 * Logger para escribir la traza de la ejecución
+	 */
+	protected static Logger log = Logger.getLogger(InterfazAdminDatos.class.getName());
+	
+	/**
+	 * Ruta al archivo de configuración de los nombres de tablas de la base de datos
+	 */
+	protected static final String CONFIG_TABLAS = "./src/main/resources/config/TablasBD.json"; 
+	
+	/* ****************************************************************
+	 * 			Atributos
+	 *****************************************************************/
+    /**
+     * Objeto JSON con los nombres de las tablas de la base de datos que se quieren utilizar
+     */
+	protected JsonObject tableConfig;
+    
+    /**
+     * Asociación a la clase principal del negocio.
+     */
+	protected Superandes superandes;
+    
+	/* ****************************************************************
+	 * 			Atributos de interfaz
+	 *****************************************************************/
+    /**
+     * Objeto JSON con la configuración de interfaz de la app.
+     */
+	protected JsonObject guiConfig;
+    
+    /**
+     * Panel de despliegue de interacción para los requerimientos
+     */
+	protected PanelDatos panelDatos;
+    
+    /**
+     * Menú de la aplicación
+     */
+	protected JMenuBar menuBar;
+
+	/* ****************************************************************
+	 * 			Métodos
+	 *****************************************************************/
+	
+	/* ****************************************************************
+	 * 			Métodos de configuración de la interfaz
+	 *****************************************************************/
+    /**
+     * Lee datos de configuración para la aplicació, a partir de un archivo JSON o con valores por defecto si hay errores.
+     * @param tipo - El tipo de configuración deseada
+     * @param archConfig - Archivo Json que contiene la configuración
+     * @return Un objeto JSON con la configuración del tipo especificado
+     * 			NULL si hay un error en el archivo.
+     */
+    protected JsonObject openConfig (String tipo, String archConfig)
+    {
+    	JsonObject config = null;
+		try 
+		{
+			Gson gson = new Gson( );
+			FileReader file = new FileReader (archConfig);
+			JsonReader reader = new JsonReader ( file );
+			config = gson.fromJson(reader, JsonObject.class);
+			log.info ("Se encontró un archivo de configuración válido: " + tipo);
+		} 
+		catch (Exception e)
+		{
+//			e.printStackTrace ();
+			log.info ("NO se encontró un archivo de configuración válido");			
+			JOptionPane.showMessageDialog(null, "No se encontró un archivo de configuración de interfaz válido: " + tipo, "Superandes App", JOptionPane.ERROR_MESSAGE);
+		}	
+        return config;
+    }
+	
+    /**
+     * Método para configurar el frame principal de la aplicación
+     */
+    protected void configurarFrame(  )
+    {
+    	int alto = 0;
+    	int ancho = 0;
+    	String titulo = "";	
+    	
+    	if ( guiConfig == null )
+    	{
+    		log.info ( "Se aplica configuración por defecto" );			
+			titulo = "Superandes APP Default";
+			alto = 300;
+			ancho = 500;
+    	}
+    	else
+    	{
+			log.info ( "Se aplica configuración indicada en el archivo de configuración" );
+    		titulo = guiConfig.get("title").getAsString();
+			alto= guiConfig.get("frameH").getAsInt();
+			ancho = guiConfig.get("frameW").getAsInt();
+    	}
+    	
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        setLocation (50,50);
+        setResizable( true );
+        setBackground( Color.WHITE );
+
+        setTitle( titulo );
+		setSize ( ancho, alto);        
+    }
+
+    /**
+     * Método para crear el menú de la aplicación con base em el objeto JSON leído
+     * Genera una barra de menú y los menús con sus respectivas opciones
+     * @param jsonMenu - Arreglo Json con los menùs deseados
+     */
+    protected void crearMenu(  JsonArray jsonMenu )
+    {    	
+    	// Creación de la barra de menús
+        menuBar = new JMenuBar();       
+        for (JsonElement men : jsonMenu)
+        {
+        	// Creación de cada uno de los menús
+        	JsonObject jom = men.getAsJsonObject(); 
+
+        	String menuTitle = jom.get("menuTitle").getAsString();        	
+        	JsonArray opciones = jom.getAsJsonArray("options");
+        	
+        	JMenu menu = new JMenu( menuTitle);
+        	
+        	for (JsonElement op : opciones)
+        	{       	
+        		// Creación de cada una de las opciones del menú
+        		JsonObject jo = op.getAsJsonObject(); 
+        		String lb =   jo.get("label").getAsString();
+        		String event = jo.get("event").getAsString();
+        		
+        		JMenuItem mItem = new JMenuItem( lb );
+        		mItem.addActionListener( this );
+        		mItem.setActionCommand(event);
+        		
+        		menu.add(mItem);
+        	}       
+        	menuBar.add( menu );
+        }        
+        setJMenuBar ( menuBar );	
+    }
+    
+    /* ****************************************************************
+	 * 			Métodos administrativos
+	 *****************************************************************/
+	/**
+	 * Muestra el log de Superandes
+	 */
+	public void mostrarLogSuperandes ()
+	{
+		mostrarArchivo ("superandes.log");
+	}
+	
+	/**
+	 * Muestra el log de datanucleus
+	 */
+	public void mostrarLogDatanuecleus ()
+	{
+		mostrarArchivo ("datanucleus.log");
+	}
+	
+	/**
+	 * Limpia el contenido del log de superandes
+	 * Muestra en el panel de datos la traza de la ejecución
+	 */
+	public void limpiarLogSuperandes ()
+	{
+		// Ejecución de la operación y recolección de los resultados
+		boolean resp = limpiarArchivo ("superandes.log");
+
+		// Generación de la cadena de caracteres con la traza de la ejecución de la demo
+		String resultado = "\n\n************ Limpiando el log de superandes ************ \n";
+		resultado += "Archivo " + (resp ? "limpiado exitosamente" : "NO PUDO ser limpiado !!");
+		resultado += "\nLimpieza terminada";
+
+		panelDatos.actualizarInterfaz(resultado);
+	}
+	
+	/**
+	 * Limpia el contenido del log de datanucleus
+	 * Muestra en el panel de datos la traza de la ejecución
+	 */
+	public void limpiarLogDatanucleus ()
+	{
+		// Ejecución de la operación y recolección de los resultados
+		boolean resp = limpiarArchivo ("datanucleus.log");
+
+		// Generación de la cadena de caracteres con la traza de la ejecución de la demo
+		String resultado = "\n\n************ Limpiando el log de datanucleus ************ \n";
+		resultado += "Archivo " + (resp ? "limpiado exitosamente" : "NO PUDO ser limpiado !!");
+		resultado += "\nLimpieza terminada";
+
+		panelDatos.actualizarInterfaz(resultado);
+	}
+	
+	/**
+	 * Limpia todas las tuplas de todas las tablas de la base de datos de superandes
+	 * Muestra en el panel de datos el número de tuplas eliminadas de cada tabla
+	 */
+	public void limpiarBD ()
+	{
+		try 
+		{
+    		// Ejecución de la demo y recolección de los resultados
+			long eliminados [] = superandes.limpiarSuperandes();
+			
+			// Generación de la cadena de caracteres con la traza de la ejecución de la demo
+			String resultado = "\n\n************ Limpiando la base de datos ************ \n";
+			resultado += eliminados [0] + " tipos de usuarios eliminados\n";
+			resultado += "\nLimpieza terminada";
+   
+			panelDatos.actualizarInterfaz(resultado);
+		} 
+		catch (Exception e) 
+		{
+//			e.printStackTrace();
+			String resultado = generarMensajeError(e);
+			panelDatos.actualizarInterfaz(resultado);
+		}
+	}
+	
+	/**
+	 * Muestra el informe del proyecto
+	 */
+	public void mostrarInforme ()
+	{
+		mostrarArchivo ("");
+	}
+	
+	/**
+	 * Muestra el modelo conceptual de Superandes
+	 */
+	public void mostrarModeloConceptual ()
+	{
+		mostrarArchivo ("");
+	}
+	
+	/**
+	 * Muestra el esquema de la base de datos de Superandes
+	 */
+	public void mostrarEsquemaBD ()
+	{
+		mostrarArchivo ("");
+	}
+	
+	/**
+	 * Muestra el script de creación de la base de datos
+	 */
+	public void mostrarScriptBD ()
+	{
+		mostrarArchivo ("");
+	}
+	
+	/**
+	 * Muestra la arquitectura de referencia para Superandes
+	 */
+	public void mostrarArqRef ()
+	{
+		mostrarArchivo ("");
+	}
+	
+	/**
+	 * Muestra la documentación Javadoc del proyecto
+	 */
+	public void mostrarJavadoc ()
+	{
+		mostrarArchivo ("");
+	}
+	
+	/**
+     * Muestra la información acerca del desarrollo de esta apicación
+     */
+    public void acercaDe ()
+    {
+		String resultado = "\n\n ************************************\n\n";
+		resultado += " * Universidad	de	los	Andes	(Bogotá	- Colombia)\n";
+		resultado += " * Departamento	de	Ingeniería	de	Sistemas	y	Computación\n";
+		resultado += " * Licenciado	bajo	el	esquema	Academic Free License versión 2.1\n";
+		resultado += " * \n";		
+		resultado += " * Curso: isis2304 - Sistemas Transaccionales\n";
+		resultado += " * Proyecto: Superandes Uniandes\n";
+		resultado += " * @version 1.0\n";
+		resultado += " * @author Sebastián Ospino\n";
+		resultado += " * @author Ana Sofía Castellanos\n";
+		resultado += " * Octubre del 2022\n";
+		resultado += " * \n";
+		resultado += " * Revisado por:\n";
+		resultado += "\n ************************************\n\n";
+
+		panelDatos.actualizarInterfaz(resultado);		
+    }
+    
+
+	/* ****************************************************************
+	 * 			Métodos privados para la presentación de resultados y otras operaciones
+	 *****************************************************************/
+
+    /**
+     * Genera una cadena de caracteres con la descripción de la excepcion e, haciendo énfasis en las excepcionsde JDO
+     * @param e - La excepción recibida
+     * @return La descripción de la excepción, cuando es javax.jdo.JDODataStoreException, "" de lo contrario
+     */
+	protected String darDetalleException(Exception e) 
+	{
+		String resp = "";
+		if (e.getClass().getName().equals("javax.jdo.JDODataStoreException"))
+		{
+			JDODataStoreException je = (javax.jdo.JDODataStoreException) e;
+			return je.getNestedExceptions() [0].getMessage();
+		}
+		return resp;
+	}
+
+	/**
+	 * Genera una cadena para indicar al usuario que hubo un error en la aplicación
+	 * @param e - La excepción generada
+	 * @return La cadena con la información de la excepción y detalles adicionales
+	 */
+	protected String generarMensajeError(Exception e) 
+	{
+		String resultado = "************ Error en la ejecución\n";
+		resultado += e.getLocalizedMessage() + ", " + darDetalleException(e);
+		resultado += "\n\nRevise datanucleus.log y superandes.log para más detalles";
+		return resultado;
+	}
+
+	/**
+	 * Limpia el contenido de un archivo dado su nombre
+	 * @param nombreArchivo - El nombre del archivo que se quiere borrar
+	 * @return true si se pudo limpiar
+	 */
+	protected boolean limpiarArchivo(String nombreArchivo) 
+	{
+		BufferedWriter bw;
+		try 
+		{
+			bw = new BufferedWriter(new FileWriter(new File (nombreArchivo)));
+			bw.write ("");
+			bw.close ();
+			return true;
+		} 
+		catch (IOException e) 
+		{
+//			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * Abre el archivo dado como parámetro con la aplicación por defecto del sistema
+	 * @param nombreArchivo - El nombre del archivo que se quiere mostrar
+	 */
+	protected void mostrarArchivo (String nombreArchivo)
+	{
+		try
+		{
+			Desktop.getDesktop().open(new File(nombreArchivo));
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/* ****************************************************************
+	 * 			Métodos de la Interacción
+	 *****************************************************************/
+    /**
+     * Método para la ejecución de los eventos que enlazan el menú con los métodos de negocio
+     * Invoca al método correspondiente según el evento recibido
+     * @param pEvento - El evento del usuario
+     */
+    @Override
+	public void actionPerformed(ActionEvent pEvento)
+	{
+		String evento = pEvento.getActionCommand( );		
+        try 
+        {
+			Method req = InterfazAdminDatos.class.getMethod ( evento );			
+			req.invoke ( this );
+		} 
+        catch (Exception e) 
+        {
+			e.printStackTrace();
+		} 
+	}
+    
+}
